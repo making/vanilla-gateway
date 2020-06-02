ed305c9926c3725082cb32ee896ee114d110717c
Toshiaki Maki <tmaki@pivotal.io>
2020-06-02 20:08:58 +0900

    first commit

---
+++ src/main/java/lol/maki/gateway/VanillaGatewayApplication.java
@@ -0,0 +1,13 @@
+package lol.maki.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanillaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanillaGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lol/maki/gateway/VanillaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package lol.maki.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanillaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
