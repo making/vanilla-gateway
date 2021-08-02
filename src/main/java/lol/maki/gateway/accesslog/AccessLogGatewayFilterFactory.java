33d3bce5a33f2dfd2b45fadecbb27d6ed81b1ab5
Toshiaki Maki <tmaki@vmware.com>
2021-08-02 22:13:06 +0900

    Skip logging for Better Uptime

--- src/main/java/lol/maki/gateway/accesslog/AccessLogGatewayFilterFactory.java
+++ src/main/java/lol/maki/gateway/accesslog/AccessLogGatewayFilterFactory.java
@@ -70,7 +70,7 @@ public class AccessLogGatewayFilterFactory extends AbstractGatewayFilterFactory<
 						final String userAgent = Objects.toString(headers.getFirst(HttpHeaders.USER_AGENT), "null");
 						final String referer = headers.getFirst(HttpHeaders.REFERER);
 						final MediaType contentType = response.getHeaders().getContentType();
-						if (userAgent.startsWith("Go-http-client")) {
+						if (userAgent.startsWith("Go-http-client") || userAgent.startsWith("Better Uptime")) {
 							return;
 						}
 						final AccessLog accessLog = new AccessLogBuilder()
